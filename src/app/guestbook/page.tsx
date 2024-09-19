fa0d83948a6567c014be4fb49c55757659c91c43
kyyril <khairilrahman55@gmail.com>
2024-09-20 05:43:34 +0700

    chore:guesbook ui

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -1,7 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function GuestBook() {
   return (
-    <div>
-      <h1>halo GuestBook</h1>
+    <section className="max-w-7xl w-full px-4 md:px-8 mx-auto">
+      <h1 className="text-4xl font-semibold lg:text-5xl pt-5">GuestBook</h1>
+      <Card className="mt-10">
+        <CardHeader className="flex flex-col w-full">
+          <Label>message</Label>
+          {<GuestBookForm />}
+        </CardHeader>
+      </Card>
+    </section>
+  );
+}
+
+function GuestBookForm() {
+  const user = false;
+  if (user) {
+    return (
+      <div>
+        <h1>halo</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="flex justify-between gap-4 flex-col md:flex-row">
+      <Input type="text" placeholder="input messege..." />
+      <Button>Submit</Button>
     </div>
   );
 }
