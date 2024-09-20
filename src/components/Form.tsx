b28ae05e2bf8103a55741e8f700f6e10cf0258f1
kyyril <khairilrahman55@gmail.com>
2024-09-20 10:56:36 +0700

    chore: connect prisma

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { useFormStatus } from "react-dom";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
 
 export function Form() {
   return (
@@ -11,6 +15,23 @@ export function Form() {
         placeholder="Input Message..."
         required
       />
+      <SubmitForm />
     </form>
+  );
+}
+
+export function SubmitForm() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled>
+          <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          Wait..
+        </Button>
+      ) : (
+        <Button type="submit">SignUp</Button>
+      )}
+    </>
   );
 }
