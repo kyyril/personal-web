b0172b3b224ab8ff3b500e1083b653d27e186fd8
kyyril <khairilrahman55@gmail.com>
2024-09-23 04:14:02 +0700

    style:loading card project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,6 +12,8 @@ import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import Link from "next/link";
 import { GlobeIcon, Github } from "lucide-react";
+import { Suspense } from "react";
+import { ProjectCardSkeleton } from "@/components/Loading";
 
 export default async function ProjectPage() {
   const data = await getJSONData();
@@ -23,63 +25,62 @@ export default async function ProjectPage() {
         <ul className="grid grid-cols-1 gap-4">
           {data.projects.map((project) => (
             <Card key={project.title} className="flex flex-col lg:flex-row">
-              <div className="w-full lg:w-1/3 flex justify-center items-center">
-                <Image
-                  src={project.cover}
-                  alt={project.title}
-                  height={200}
-                  width={300}
-                  className="rounded-md object-cover my-4"
-                />
-              </div>
-
-              <div className="w-full lg:w-2/3">
-                <CardHeader className="py-2 px-6">
-                  <CardTitle className="text-lg pt-3 font-semibold">
-                    {project.title}
-                  </CardTitle>
-                  <div className="flex flex-wrap gap-1 mt-1">
-                    {project.technologies.map((tech) => (
-                      <Badge key={tech} variant="secondary">
-                        {tech}
-                      </Badge>
-                    ))}
-                  </div>
-                </CardHeader>
-
-                <CardContent className="py-1">
-                  <CardDescription>{project.description}</CardDescription>
-                </CardContent>
-
-                <CardFooter className="py-2 pb-4">
-                  <div className="flex space-x-2">
-                    <Link
-                      target="_blank"
-                      href={project.live_url}
-                      prefetch={false}
-                    >
-                      <Button size="sm" className="hover:text-violet-500">
-                        <GlobeIcon className="h-3 w-3 mr-1" />
-                        Demo
-                      </Button>
-                    </Link>
-                    <Link
-                      target="_blank"
-                      href={project.code_repo_url}
-                      prefetch={false}
-                    >
-                      <Button
-                        size="sm"
-                        className="hover:text-violet-500"
-                        variant="outline"
+              <Suspense fallback={<ProjectCardSkeleton />}>
+                <div className="w-full lg:w-1/3 flex justify-center items-center">
+                  <Image
+                    src={project.cover}
+                    alt={project.title}
+                    height={200}
+                    width={300}
+                    className="rounded-md object-cover my-4"
+                  />
+                </div>
+                <div className="w-full lg:w-2/3">
+                  <CardHeader className="py-2 px-6">
+                    <CardTitle className="text-lg pt-3 font-semibold">
+                      {project.title}
+                    </CardTitle>
+                    <div className="flex flex-wrap gap-1 mt-1">
+                      {project.technologies.map((tech) => (
+                        <Badge key={tech} variant="secondary">
+                          {tech}
+                        </Badge>
+                      ))}
+                    </div>
+                  </CardHeader>
+                  <CardContent className="py-1">
+                    <CardDescription>{project.description}</CardDescription>
+                  </CardContent>
+                  <CardFooter className="py-2 pb-4">
+                    <div className="flex space-x-2">
+                      <Link
+                        target="_blank"
+                        href={project.live_url}
+                        prefetch={false}
                       >
-                        <Github className="h-3 w-3 mr-1" />
-                        Repo
-                      </Button>
-                    </Link>
-                  </div>
-                </CardFooter>
-              </div>
+                        <Button size="sm" className="hover:text-violet-500">
+                          <GlobeIcon className="h-3 w-3 mr-1" />
+                          Demo
+                        </Button>
+                      </Link>
+                      <Link
+                        target="_blank"
+                        href={project.code_repo_url}
+                        prefetch={false}
+                      >
+                        <Button
+                          size="sm"
+                          className="hover:text-violet-500"
+                          variant="outline"
+                        >
+                          <Github className="h-3 w-3 mr-1" />
+                          Repo
+                        </Button>
+                      </Link>
+                    </div>
+                  </CardFooter>
+                </div>
+              </Suspense>
             </Card>
           ))}
         </ul>
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -63,3 +63,35 @@ export function GuestBookLoadingForm() {
     </div>
   );
 }
+
+export function ProjectCardSkeleton() {
+  return (
+    <>
+      <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4">
+        <div className="w-full lg:w-1/3 flex justify-center items-center">
+          <Skeleton className="h-[200px] w-[300px] rounded-md my-4" />{" "}
+          {/* Skeleton for the image */}
+        </div>
+        <div className="w-full lg:w-2/3 space-y-3">
+          <div className="py-2 px-6">
+            <Skeleton className="h-6 w-1/2" /> {/* Skeleton for the title */}
+            <div className="flex flex-wrap gap-1 mt-1">
+              <Skeleton className="h-6 w-16" />{" "}
+              {/* Skeleton for technologies */}
+              <Skeleton className="h-6 w-16" />
+              <Skeleton className="h-6 w-16" />
+            </div>
+          </div>
+          <div className="py-1">
+            <Skeleton className="h-5 w-full" /> {/* Skeleton for description */}
+            <Skeleton className="h-5 w-3/4 mt-2" />
+          </div>
+          <div className="py-2 pb-4 flex space-x-2">
+            <Skeleton className="h-10 w-20" /> {/* Skeleton for Demo button */}
+            <Skeleton className="h-10 w-20" /> {/* Skeleton for Repo button */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
